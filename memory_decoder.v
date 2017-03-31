ce7f905a7f15ef0bf726c77e3a003b53a8b86734
zicog <mrychlik@rsm.ie>
2017-03-31 03:07:53 +0300

    Tweaked decoder logic

--- memory_decoder.v
+++ memory_decoder.v
@@ -1,11 +1,23 @@
 //
 // memory_decoder
 //
-// Here is where we diced what devices on what addresses get enabled.
+// Here is where we decide what devices on what addresses get enabled.
 //
 
 module memory_decoder (input [31:0] address, output [7:0] enables);
-    assign enables[0] = 'b1;
+    integer memoryEnable;
+    always @(address) begin
+        memoryEnable = 0;
+        if (address[31]) begin
+            // A peripheral device access.
+        end else begin
+            // A memory access.
+            memoryEnable = 1;
+        end
+    end
+    assign enables[0] = memoryEnable;
+/*
+    assign enables[0] = !(address & 'h80000000);
     assign enables[1] = 'b0;
     assign enables[2] = 'b0;
     assign enables[3] = 'b0;
@@ -13,4 +25,5 @@ module memory_decoder (input [31:0] address, output [7:0] enables);
     assign enables[5] = 'b0;
     assign enables[6] = 'b0;
     assign enables[7] = 'b0;
+*/
 endmodule
