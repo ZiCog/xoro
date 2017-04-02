b09a47c0cc944a06b616c1f8ace5f2d276d5b3ad
zicog <mrychlik@rsm.ie>
2017-04-02 15:49:01 +0300

    Moved the verilog to the rtl directory

--- tests/load-store.c
+++ tests/load-store.c
@@ -2,6 +2,7 @@
 
 int main(int argc, char* argv[])
 {
+start:
     *(uint8_t*) 0x100 = 0x10;
     *(uint8_t*) 0x101 = 0x11;
     *(uint8_t*) 0x102 = 0x12;
@@ -11,6 +12,12 @@ int main(int argc, char* argv[])
 
     *(uint16_t*) 0x100 = 0x1110;
     *(uint16_t*) 0x102 = 0x1312;
+
+    int32_t count = 0;
+loop:
+    *(uint32_t*) 0xffff0060 = count >> 8;
+    count = count + 1;
+    goto loop;
 
     return 0;
 }
