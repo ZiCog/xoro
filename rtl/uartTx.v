49796f1c8e441ef338524334ed8571ff2f28d26f
zicog <mrychlik@rsm.ie>
2017-04-06 15:56:26 +0300

    uartTX interface is now a picorv32 memory interface

--- rtl/uartTx.v
+++ rtl/uartTx.v
@@ -1,30 +1,40 @@
 //
-// Design Name : uartTx 
+// Design Name : uartTx
 // File Name   : uartTx.v
 // Function    : Simple UART transmiter.
 //               115200 baud when driven from 50Mhz clock.
-//               Single byte buffer for concurrent loading while tramitting.                
+//               Single byte buffer for concurrent loading while tramitting.
 // Coder       : Heater.
 //
 
 module uartTx (
-    input       resn,          // Reset, active low.
-    input       clk,           // Clock, (Use 50Mhz for 115200 baud).
-    input       wr,            // Writes data to holding register on rising clock
-    input [7:0] data,          // Data to be transmitted.
-    output reg  serialOut,     // The serial outout.
-    output reg  empty          // TRUE when ready to accept next character.
+    // Bus interface
+    input  wire        clk,
+    input  wire        resetn,
+    input  wire        enable,
+    input  wire        mem_valid,
+    output wire        mem_ready,
+    input  wire        mem_instr,
+    input  wire [3:0]  mem_wstrb,
+    input  wire [31:0] mem_wdata,
+    input  wire [31:0] mem_addr,
+    output wire [31:0] mem_rdata,
+
+    // Serial interface
+    output reg  serialOut     // The serial outout.
 );
-    // Internal Variables 
+    // Internal Variables
     reg [7:0]  shifter;
     reg [7:0]  buffer;
     reg [7:0]  state;
     reg [3:0]  bitCount;
-    reg [19:0] bitTimer; 
+    reg [19:0] bitTimer;
+    reg        empty;          // TRUE when ready to accept next character.
+    reg        rdy;
 
     // UART TX Logic
-    always @ (posedge clk or negedge resn) begin
-        if (!resn) begin
+    always @ (posedge clk or negedge resetn) begin
+        if (!resetn) begin
             state     <= 0;
             buffer    <= 0;
             empty     <= 1;
@@ -32,10 +42,17 @@ module uartTx (
             serialOut <= 1;
             bitCount  <= 0;
             bitTimer  <= 0;
+            empty     <= 0;
+            rdy       <= 0;
         end else begin
-            if ((wr == 1) && (empty == 1)) begin
-                buffer <= data;
-                empty <= 0;
+            if (mem_valid & enable) begin
+                if  ((mem_wstrb[0] == 1) && (empty == 1)) begin
+                    buffer <= mem_wdata;
+                    empty <= 0;
+                end;
+                rdy <= 1;
+            end else begin
+                rdy <= 0;
             end
 
             // Generate bit clock timer for 115200 baud from 50MHz clock
@@ -76,4 +93,9 @@ module uartTx (
             end
         end
     end
+
+    // Tri-state the bus outputs.
+    assign mem_rdata = enable ? empty : 'bz;
+    assign mem_ready = enable ? rdy : 'bz;
+
 endmodule
--- rtl/xoro_top.v
+++ rtl/xoro_top.v
@@ -1,35 +1,8 @@
 //
-// blinky
+// xoro_top.v
 //
-
-// DE0-Nano pin outs.
-// Signal Name FPGA Pin No. Description
-// CLOCK_50    R8           50MHz Clock input
-
-// Signal Name FPGA Pin No. Description
-// KEY[0]      PIN_J15      Push-button[0] 3.3V
-// KEY[1]      PIN_E1       Push-button[1] 3.3V
-
-// Signal Name FPGA Pin No. Description
-// LED[0]      PIN_A15      LED Green[0] 3.3V
-// LED[1]      PIN_A13      LED Green[1] 3.3V
-// LED[2]      PIN_B13      LED Green[2] 3.3V
-// LED[3]      PIN_A11      LED Green[3] 3.3V
-// LED[4]      PIN_D1       LED Green[4] 3.3V
-// LED[5]      PIN_F3       LED Green[5] 3.3V
-// LED[6]      PIN_B1       LED Green[6] 3.3V
-// LED[7]      PIN_L3       LED Green[7] 3.3V
-
-// Signal Name FPGA Pin No. Description
-// GPIO_10     PIN_F13      GPIO JP2, 2
-// GPIO_11     PIN_T15      GPIO JP2, 4
-// GPIO_13     PIN_T13      GPIO JP2, 6
-// GPIO_15     PIN_T12      GPIO JP2, 8
-
-// Signal Name FPGA Pin No. Description
-// GPIO_17     PIN_T11      GPIO JP2, 10
-
-
+// For all DE0-Nano pin outs the file DE0_NANO.qsf
+//
 module xoro_top (input CLOCK_50, input reset_btn, output[7:0] LED, output[3:0] RND_OUT, output UART_TX);
 
     wire        trap;
@@ -94,6 +67,18 @@ module xoro_top (input CLOCK_50, input reset_btn, output[7:0] LED, output[3:0] R
         .locked (CLOCK_LOCKED)     // PLL is locked signal
     );
 
+    memory mem (
+        .clk(CLOCK_100),
+        .enable(enables[7]),
+        .mem_valid(mem_valid),
+        .mem_ready(mem_ready),
+        .mem_instr(mem_instr),
+        .mem_wstrb(mem_wstrb),
+        .mem_wdata(mem_wdata),
+        .mem_addr(mem_addr),
+        .mem_rdata(mem_rdata)
+    );
+
     gpio gpio (
         .clk(CLOCK_100),
         .resetn(resetn),
@@ -121,16 +106,18 @@ module xoro_top (input CLOCK_50, input reset_btn, output[7:0] LED, output[3:0] R
         .mem_rdata(mem_rdata)
     );
 
-    memory mem (
+    uartTx utx (
         .clk(CLOCK_100),
-        .enable(enables[7]),
+        .resetn(resetn),
+        .enable(enables[4]),
         .mem_valid(mem_valid),
         .mem_ready(mem_ready),
         .mem_instr(mem_instr),
         .mem_wstrb(mem_wstrb),
         .mem_wdata(mem_wdata),
         .mem_addr(mem_addr),
-        .mem_rdata(mem_rdata)
+        .mem_rdata(mem_rdata),
+        .serialOut(UART_TX)
     );
 
     address_decoder ad (
