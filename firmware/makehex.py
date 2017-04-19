96d36a35d9a84c52250a334479110c2274f8077f
zicog <mrychlik@rsm.ie>
2017-04-19 22:39:41 +0300

    makehex now also makes a .mif file

--- firmware/makehex.py
+++ firmware/makehex.py
@@ -21,10 +21,22 @@ hexfile1 = "firmware1.hex"
 hexfile2 = "firmware2.hex"
 hexfile3 = "firmware3.hex"
 
+miffile = "firmware.mif"
+
 h0 = open(path + hexfile0, "w")
 h1 = open(path + hexfile1, "w")
 h2 = open(path + hexfile2, "w")
 h3 = open(path + hexfile3, "w")
+mif = open(path + miffile, "w")
+
+print ("-- Altera Quartus Memory Initilization file .mif",          file=mif)
+print ("DEPTH = 16384;         -- The size of memory in words",     file=mif)
+print ("WIDTH = 32;            -- The size of data in bits",        file=mif)
+print ("",                                                          file=mif)
+print ("ADDRESS_RADIX = HEX;   -- The radix for address values",    file=mif)
+print ("DATA_RADIX = HEX;      -- The radix for data values",       file=mif)
+print ("",                                                          file=mif)
+print ("CONTENT BEGIN          -- start of (address : data pairs)", file=mif)
 
 assert len(bindata) < 4*nwords
 assert len(bindata) % 4 == 0
@@ -37,14 +49,19 @@ for i in range(nwords):
         print("%02x" % (w[1]), file=h1)
         print("%02x" % (w[2]), file=h2)
         print("%02x" % (w[3]), file=h3)
+        print("\t%04x : %08x;" % (i  ,(w[3] << 24) + (w[2] << 16) + (w[1] << 8) + w[0]) , file=mif)
     else:
         print("0")
-        print("%02x" % (0), file=h0)
-        print("%02x" % (0), file=h1)
-        print("%02x" % (0), file=h2)
-        print("%02x" % (0), file=h3)
+        print("%02x" % (0),            file=h0)
+        print("%02x" % (0),            file=h1)
+        print("%02x" % (0),            file=h2)
+        print("%02x" % (0),            file=h3)
+        print("\t%04x : %08x;" % (0, 0), file=mif)
+
+print ("END;", file=mif)
 
 h0.close()
 h1.close()
 h2.close()
 h3.close()
+mif.close()
